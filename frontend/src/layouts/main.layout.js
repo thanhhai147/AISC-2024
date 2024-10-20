483e3bb58e65fc005b9713df4ffdba0449b67a33
thanhhai147 <thanhhai252004@gmail.com>
2024-10-20 22:01:29 +0700

    add click and hover effect for navbar question component

--- frontend/src/layouts/main.layout.js
+++ frontend/src/layouts/main.layout.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import Sidebar from '../components/sidebar.component'
+import NavbarQuestion from '../components/navbarQuestion.component'
 
 export default function MainLayout({children}) {
     return (
