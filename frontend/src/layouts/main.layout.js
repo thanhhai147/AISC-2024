a063890da6db93004934f04b3357e6f8aa96e15e
snnasnguyen <haidangnguyen467@gmail.com>
2024-10-22 17:43:29 +0700

    add OTP component

--- frontend/src/layouts/main.layout.js
+++ frontend/src/layouts/main.layout.js
@@ -1,8 +1,11 @@
 import React from 'react'
-
+import OTP from '../components/OTP.component.js'
+import NavbarQuestion from '../components/navbarQuestion.component.js'
 export default function MainLayout({children}) {
     return (
         <>
+            <OTP/>
+            <NavbarQuestion/>
             {children}
         </>
     )
