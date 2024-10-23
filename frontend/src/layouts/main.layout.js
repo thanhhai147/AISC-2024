fb15cd9a7a09ec5bc749783ed8238c2b4d8c3ebd
tubinguyen <145632002+tubinguyen@users.noreply.github.com>
2024-10-23 13:32:22 +0700

    fileUploadButton

--- frontend/src/layouts/main.layout.js
+++ frontend/src/layouts/main.layout.js
@@ -3,13 +3,17 @@ import React from 'react'
 import Sidebar from '../components/sidebar.component'
 import SearchBar from '../components/searchbar.component'
 import NavbarAccount from '../components/navbarAccount.component'
+import ListItems from '../components/listItems.component'
+import FileUploadButton from '../components/fileUploadButton.component'
 
 export default function MainLayout({children}) {
     return (
         <>
             <Sidebar />
-            <SearchBar />
-            <NavbarAccount />
+            {/* <SearchBar /> */}
+            {/* <NavbarAccount /> */}
+            {/* <ListItems/> */}
+            <FileUploadButton/>
             {children}
         </>
     )
