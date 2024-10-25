2a112266ae6d3a58b468b3c95f2288deb5a27380
thanhhai147 <thanhhai252004@gmail.com>
2024-10-25 10:59:27 +0700

    Update file names in FE

--- frontend/src/components/DocumentUploadBox.component.js
+++ frontend/src/components/DocumentUploadBox.component.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import '../assets/css/DocumentUploadBox.css';
+import '../assets/css/documentUploadBox.css';
 import { IoDocumentTextOutline } from "react-icons/io5";
 import { HiOutlineUpload } from "react-icons/hi";
 import Button from './button.component';
