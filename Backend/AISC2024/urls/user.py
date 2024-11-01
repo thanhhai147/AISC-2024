3de5c411c11307f29b7339260c78ff12a5c66ace
thanhhai147 <thanhhai252004@gmail.com>
2024-11-01 20:35:38 +0700

    Add updated_at for signup

--- Backend/AISC2024/urls/user.py
+++ Backend/AISC2024/urls/user.py
@@ -2,5 +2,5 @@ from django.urls import path
 from ..views.user import SignUpAPIView
 
 urlpatterns = [
-    path('signup', SignUpAPIView.as_view(), name='query'),
+    path('signup', SignUpAPIView.as_view(), name='signup')
 ]
