9673b423d030dc61d9741560e210ae693d34959f
tubinguyen <145632002+tubinguyen@users.noreply.github.com>
2024-11-02 19:35:27 +0700

    post

--- frontend/src/layouts/main.layout.js
+++ frontend/src/layouts/main.layout.js
@@ -1,20 +1,21 @@
 import React  from 'react'
-
-import BackButton from '../components/buttonBack.component'
-import GoogleLoginButton from '../components/buttonLoginGoogle.component'
-import OTP from '../components/OTP.component'
-import IconPremium from '../components/iconPremium.component'
-import DocumentUploadBox from '../components/documentUploadBox.component'
-import FunctionBoxes from '../components/functionBoxes.component'
-import RecentlyActiveBox from '../components/recentlyActiveBox.component'
-import NavbarHomePage from '../components/navbarHomePage.component'
-import NavbarExam from '../components/navbarExam.component'
+import { useState } from 'react'
+// import BackButton from '../components/buttonBack.component'
+// import GoogleLoginButton from '../components/buttonLoginGoogle.component'
+// import OTP from '../components/OTP.component'
+// import IconPremium from '../components/iconPremium.component'
+// import DocumentUploadBox from '../components/documentUploadBox.component'
+// import FunctionBoxes from '../components/functionBoxes.component'
+// import RecentlyActiveBox from '../components/recentlyActiveBox.component'
+// import NavbarHomePage from '../components/navbarHomePage.component'
+// import NavbarExam from '../components/navbarExam.component'
+import TextInput from '../components/textInput.component'
 
 export default function MainLayout({children}) {
     return (
         <>
-            <BackButton />
-
+            {/* <BackButton /> */}
+            <TextInput />
             {children}
         </>
     )
