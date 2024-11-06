dc84da51c5b3e468e91cf3991b5c90c26baf4ba7
thanhhai147 <thanhhai252004@gmail.com>
2024-11-06 08:54:01 +0700

    reset main layout

--- frontend/src/layouts/main.layout.js
+++ frontend/src/layouts/main.layout.js
@@ -7,9 +7,6 @@ import Post from '../components/post.component';
 import Comment from '../components/comment.component';
 
 export default function MainLayout({ children }) {
-    const imageUrl = require('../assets/img/Post.png'); 
-    const content = "Đây là nội dung bài viết mẫu. Bạn có thể thay thế bằng nội dung thực tế.";
-    const timestamp = new Date().toLocaleString();
 
     return (
         <>
