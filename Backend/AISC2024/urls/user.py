1a47d416c372f8dbf488a0f97977ef95a61e1c61
Lê Ngọc Thiên Phúc <thienphucle27@gmail.com>
2024-11-10 19:38:52 +0700

    updateUser

--- Backend/AISC2024/urls/user.py
+++ Backend/AISC2024/urls/user.py
@@ -2,10 +2,12 @@ from django.urls import path
 from ..views.user import SignUpAPIView
 from ..views.user import LogInAPIView
 from ..views.user import LogOutAPIView
+from ..views.user import UpdateAPIView
 
 
 urlpatterns = [
     path('signup', SignUpAPIView.as_view(), name='signup'),
     path('login', LogInAPIView.as_view(), name='login'),
-    path('logout', LogOutAPIView.as_view(), name='logout')
+    path('logout', LogOutAPIView.as_view(), name='logout'),
+    path('update', UpdateAPIView.as_view(), name='update')
 ]
