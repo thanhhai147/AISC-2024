e68fada211fc63bdeddedf36001c29450322a730
thanhhai147 <thanhhai252004@gmail.com>
2024-11-10 16:35:34 +0700

    Setting url for quizzes

--- Backend/Backend/urls.py
+++ Backend/Backend/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('AISC2024.urls.user'))
+    path('', include('AISC2024.urls.user')),
+    path('', include('AISC2024.urls.quizzes'))
 ]
